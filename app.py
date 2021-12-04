0bc75f53b6d0e201bd05fbb4e4615081c60d2915
saitejachitti <saitejach093@gmail.com>
2021-12-04 15:49:50 +0530

    question C

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from resources.emp import Yahoo,Benchmark
+from resources.emp import Yahoo,Benchmark,Trade
 from flask_cors import CORS
 
 app=Flask(__name__)
@@ -10,6 +10,6 @@ api=Api(app)
 CORS(app)
 api.add_resource(Yahoo,'/yahoo')
 api.add_resource(Benchmark,'/benchmark')
-
+api.add_resource(Trade,'/trade')
 if __name__=='__main__':
     app.run()
--- resources/emp.py
+++ resources/emp.py
@@ -85,3 +85,44 @@ class Benchmark(Resource):
             return {"data":l2},201
         except:
             return {"message":"There was an error connecting to user table."},500
+
+
+class Trade(Resource):
+    def get(self):
+        try:
+            url = "https://yfapi.net/v8/finance/spark?interval=1mo&range=8y&symbols=AAPL%2CMSFT%2CAMZN%2CFB%2CTSLA%2CNDX"
+
+            querystring = {"symbols":"AAPL,MSFT,AMZN,FB,TSLA,NDX"}
+
+            headers = {
+                'x-api-key': "bu1LFtkDIT3Dy3HsaBwnv95yRFyATHIf6sWFzDs6"
+                }
+
+            response = requests.request("GET", url, headers=headers, params=querystring)
+
+
+            data=json.loads(response.text)
+            stocks=['AAPL','MSFT','AMZN','FB','TSLA','NDX']
+            dic={}
+            for i in stocks:
+                for j in range(len(data[i]['timestamp'])):
+                    data[i]['timestamp'][j]=dt.datetime.utcfromtimestamp(data[i]['timestamp'][j]).strftime("%m/%Y")
+                l1=[]
+                l2=[]
+                d={}
+                d1=dt.date(2015,12,1)
+                d2=dt.date(2021,1,1)
+                for k,v in zip(data[i]['timestamp'],data[i]['close']):
+                    l=k.split("/")
+                    p=dt.date(int(l[1]),int(l[0]),1) # Converting String date to Date Object  
+                    #print(d1,d2)
+                    if p>= d1 and p <= d2:
+                        l1.append(k)
+                        l2.append(v)
+                        print(k,v)
+                #d['datetime']=l1
+                d['close']=l2
+                dic[i]=d
+            return {"data":dic},201
+        except:
+            return {"message":"There was an error connecting to user table."},500
