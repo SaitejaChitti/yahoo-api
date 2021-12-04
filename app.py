894b04e565b7ef6b3588c31934f0b4a20673378f
saitejachitti <saitejach093@gmail.com>
2021-12-04 14:33:02 +0530

    Question A

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api
+from resources.emp import Profile,Age,Chart
+from flask_cors import CORS
+
+app=Flask(__name__)
+
+app.config['PROPAGATE_EXCEPTIONS']=True
+api=Api(app)
+CORS(app)
+api.add_resource(Yahoo,'/yahoo')
+
+
+if __name__=='__main__':
+    app.run()
