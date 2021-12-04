745d09462476587363518ef08ada4a5ceb74015b
saitejachitti <saitejach093@gmail.com>
2021-12-04 15:03:45 +0530

    Question B

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from resources.emp import Yahoo
+from resources.emp import Yahoo,Benchmark
 from flask_cors import CORS
 
 app=Flask(__name__)
@@ -9,7 +9,7 @@ app.config['PROPAGATE_EXCEPTIONS']=True
 api=Api(app)
 CORS(app)
 api.add_resource(Yahoo,'/yahoo')
-
+api.add_resource(Benchmark,'/benchmark')
 
 if __name__=='__main__':
     app.run()
